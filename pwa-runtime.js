0827f913902d8528d8f930b3051980e5266785c8
César Mátzar <evolutiongt01@gmail.com>
2026-08-19 17:47:47 -0600

    Add files via upload

--- pwa-runtime.js
+++ pwa-runtime.js
@@ -1,6 +1,6 @@
 (() => {
   'use strict';
-  const VERSION = '56';
+  const VERSION = '57';
   const SW_URL = `/sw.js?v=${VERSION}`;
   const PROMPT_KEY = 'dingloft_update_prompted_version';
   const PROMPT_TIME_KEY = 'dingloft_update_prompted_at';
