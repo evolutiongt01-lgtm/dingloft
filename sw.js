2011a7446b69532e5a2a112ac035a176a6a4b522
César Mátzar <evolutiongt01@gmail.com>
2026-09-11 09:15:52 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '116';
+const VERSION = '117';
 const CACHE_PREFIX = 'dingloft-app-';
 const CACHE = `${CACHE_PREFIX}v${VERSION}-offline`;
 const RUNTIME = `${CACHE_PREFIX}runtime-v${VERSION}`;
@@ -61,7 +61,7 @@ const CORE = [
   '/pwa-install.js',
   '/dingloft-commerce.js?v=2.2.1-shell94',
   '/dingloft-cart-sync.js?v=91',
-  '/pwa-runtime.js?v=79',
+  '/pwa-runtime.js?v=80',
   '/dingloft-mobile-nav-v71.js?v=101',
   '/dingloft-mobile-dock.css?v=34',
   '/dingloft-mobile-cart-v92.js?v=95',
