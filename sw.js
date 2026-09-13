6706dce417ab7a6a8ad5ce3a45ee665bd1e7b7c9
César Mátzar <evolutiongt01@gmail.com>
2026-09-12 22:17:00 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = '165';
+const VERSION = '166';
 const CACHE_PREFIX = 'dingloft-app-';
 const CACHE = `${CACHE_PREFIX}v${VERSION}-offline`;
 const RUNTIME = `${CACHE_PREFIX}runtime-v${VERSION}`;
